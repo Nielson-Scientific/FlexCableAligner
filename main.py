74c74da773e7c1fbf63a19ed42c72c572c20442e
Christopher Chinh <christopherchinh@gmail.com>
2025-07-25 13:57:56 -0600

    Refactoring

---
+++ main.py
@@ -0,0 +1,8 @@
+from include.AsyncSmoothJoystickController import AsyncSmoothJoystickController
+
+def main():
+    app = AsyncSmoothJoystickController()
+    app.run()
+
+if __name__ == "__main__":
+    main()
