169658f5776a7d3402627cfad4ec3e51c2a4a0cc
AaronBencomo <baaronm@student.byu.edu>
2025-08-27 15:52:27 -0600

    highlight fix

--- include/gui.py
+++ include/gui.py
@@ -635,7 +635,7 @@ class FlexAlignerGUI:
 
         # Highlight the selected row
         for e in self.row_list[self.selected_row_index]:
-            e.config(state='readonly', background='lightblue')
+            e.config(state='readonly', readonlybackground='lightblue')
             # if its the third row, don't change to readonly, but do change its color
 
 
